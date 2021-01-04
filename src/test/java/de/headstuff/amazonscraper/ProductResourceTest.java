280a8e9f5fdca521ee19f7f59e4bf2e05df7adf4
Thimo Koenig <thimo.koenig@gmail.com>
2021-01-04 11:29:15 +0100

    Minor clean up in application profile; Fixes in Dockerfile for native image builds

--- src/test/java/de/headstuff/amazonscraper/ProductResourceTest.java
+++ src/test/java/de/headstuff/amazonscraper/ProductResourceTest.java
@@ -1,40 +1,21 @@
 package de.headstuff.amazonscraper;
 
+import static io.restassured.RestAssured.given;
+
 import io.quarkus.test.junit.QuarkusTest;
-import lombok.val;
 import org.junit.jupiter.api.Test;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.Locale;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @QuarkusTest
-public class ProductResourceTest {
+class ProductResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello RESTEasy"));
-    }
+  // TODO: think about Testcontainers with Mongo DB running
 
-    @Test
-    public void testRegex() throws ParseException {
-        String searchString = "1,150";
-        NumberFormat format = NumberFormat.getInstance(Locale.GERMAN);
-        val target = format.parse(searchString).doubleValue();
-        assertEquals(target, 1.150);
-    }
-
-
-
+  @Test
+  void testProductsEndpoint() {
+    given()
+        .when().get("/products")
+        .then()
+        .statusCode(200);
+  }
 
 }
